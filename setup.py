3803a0f6f3301437f9a924da732e89c6976a8eda
Aztechiot <warren.lee@aztech.com>
2020-09-08 14:45:11 +0800

    adding files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='pyaztech',
+      version='0.3.5',
+      description='Interface for TPLink HS1xx plugs, HS2xx wall switches & LB1xx bulbs',
+      url='https://github.com/Aztechiot/pyaztech',
+      author='Aztechiot',
+      author_email='warren.lee@aztech.com',
+      license='GPLv3',
+      packages=['pyaztech'],
+      install_requires=['click', 'deprecation'],
+      python_requires='>=3.5',
+      entry_points={
+            'console_scripts': [
+                  'pyaztech=pyaztech.cli:cli',
+            ],
+      },
+      zip_safe=False)
